d928847c65f74a5c735f8c22ab0e0c009b6f642f
Raker <gitrustz10@gmail.com>
2022-11-12 04:12:14 -0500

    update

--- lua/keymap/remap.lua
+++ lua/keymap/remap.lua
@@ -1,4 +1,4 @@
-local keymap = require('core.keymap')
+local keymap = require("core.keymap")
 local nmap, imap, cmap = keymap.nmap, keymap.imap, keymap.cmap
 local silent, noremap = keymap.silent, keymap.noremap
 local expr = keymap.expr
@@ -7,55 +7,55 @@ local cmd = keymap.cmd
 
 -- noremal remap
 nmap({
-  -- close buffer
-  { '<C-x>k', cmd('bdelete'), opts(noremap, silent) },
-  -- save
-  { '<C-s>', cmd('write'), opts(noremap) },
-  -- yank
-  { 'Y', 'y$', opts(noremap) },
-  -- buffer jump
-  { ']b', cmd('bp'), opts(noremap) },
-  { '[b', cmd('bp'), opts(noremap) },
-  -- remove trailing white space
-  { '<Leader>t', cmd('TrimTrailingWhitespace'), opts(noremap) },
-  -- window jump
-  { '<C-h>', '<C-w>h', opts(noremap) },
-  { '<C-l>', '<C-w>l', opts(noremap) },
-  { '<C-j>', '<C-w>j', opts(noremap) },
-  { '<C-k>', '<C-w>k', opts(noremap) },
-  { '<esc>', cmd('noh'), opts(noremap) },
-  -- resize window
-  { '<A-[>', cmd('vertical resize -5'), opts(noremap, silent) },
-  { '<A-]>', cmd('vertical resize +5'), opts(noremap, silent) },
+	-- close buffer
+	{ "<C-x>k", cmd("bdelete"), opts(noremap, silent) },
+	-- save
+	{ "<C-s>", cmd("write"), opts(noremap) },
+	-- yank
+	{ "Y", "y$", opts(noremap) },
+	-- buffer jump
+	{ "]b", cmd("bp"), opts(noremap) },
+	{ "[b", cmd("bp"), opts(noremap) },
+	-- remove trailing white space
+	{ "<Leader>t", cmd("TrimTrailingWhitespace"), opts(noremap) },
+	-- window jump
+	{ "<C-h>", "<C-w>h", opts(noremap) },
+	{ "<C-l>", "<C-w>l", opts(noremap) },
+	{ "<C-j>", "<C-w>j", opts(noremap) },
+	{ "<C-k>", "<C-w>k", opts(noremap) },
+	{ "<esc>", cmd("noh"), opts(noremap) },
+	-- resize window
+	{ "<A-[>", cmd("vertical resize -5"), opts(noremap, silent) },
+	{ "<A-]>", cmd("vertical resize +5"), opts(noremap, silent) },
 })
 
 -- insertmode remap
 imap({
-  { '<C-w>', '<C-[>diwa', opts(noremap) },
-  { '<C-h>', '<Bs>', opts(noremap) },
-  { '<C-d>', '<Del>', opts(noremap) },
-  { '<C-u>', '<C-G>u<C-u>', opts(noremap) },
-  { '<C-b>', '<Left>', opts(noremap) },
-  { '<C-f>', '<Right>', opts(noremap) },
-  { '<C-a>', '<Esc>^i', opts(noremap) },
-  { '<C-j>', '<Esc>o', opts(noremap) },
-  { '<C-k>', '<Esc>O', opts(noremap) },
-  { '<C-s>', '<ESC>:w<CR>', opts(noremap) },
-  {
-    '<C-e>',
-    function()
-      return vim.fn.pumvisible() == 1 and '<C-e>' or '<end>'
-    end,
-    opts(expr),
-  },
+	{ "<C-w>", "<C-[>diwa", opts(noremap) },
+	{ "<C-h>", "<Bs>", opts(noremap) },
+	{ "<C-d>", "<Del>", opts(noremap) },
+	{ "<C-u>", "<C-G>u<C-u>", opts(noremap) },
+	{ "<C-b>", "<Left>", opts(noremap) },
+	{ "<C-f>", "<Right>", opts(noremap) },
+	{ "<C-a>", "<Esc>^i", opts(noremap) },
+	{ "<C-j>", "<Esc>o", opts(noremap) },
+	{ "<C-k>", "<Esc>O", opts(noremap) },
+	{ "<C-s>", "<ESC>:w<CR>", opts(noremap) },
+	{
+		"<C-e>",
+		function()
+			return vim.fn.pumvisible() == 1 and "<C-e>" or "<end>"
+		end,
+		opts(expr),
+	},
 })
 
 -- commandline remap
 cmap({
-  { '<C-b>', '<Left>', opts(noremap) },
-  { '<C-f>', '<Right>', opts(noremap) },
-  { '<C-a>', '<Home>', opts(noremap) },
-  { '<C-e>', '<End>', opts(noremap) },
-  { '<C-d>', '<Del>', opts(noremap) },
-  { '<C-h>', '<BS>', opts(noremap) },
+	{ "<C-b>", "<Left>", opts(noremap) },
+	{ "<C-f>", "<Right>", opts(noremap) },
+	{ "<C-a>", "<Home>", opts(noremap) },
+	{ "<C-e>", "<End>", opts(noremap) },
+	{ "<C-d>", "<Del>", opts(noremap) },
+	{ "<C-h>", "<BS>", opts(noremap) },
 })
--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -1,53 +1,53 @@
-local package = require('core.pack').package
-local conf = require('modules.ui.config')
+local package = require("core.pack").package
+local conf = require("modules.ui.config")
 
 package({
-  'RakerZh/zephyr-nvim',
-  config = conf.zephyr,
-  requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
+	"RakerZh/zephyr-nvim",
+	config = conf.zephyr,
+	requires = { "nvim-treesitter/nvim-treesitter", opt = true },
 })
 
 package({
-  'folke/tokyonight.nvim',
+	"folke/tokyonight.nvim",
 })
 
-package({ 'kyazdani42/nvim-web-devicons' })
+package({ "kyazdani42/nvim-web-devicons" })
 
-package({ 'glepnir/dashboard-nvim', config = conf.dashboard })
-
-package({
-  'glepnir/galaxyline.nvim',
-  branch = 'main',
-  config = conf.galaxyline,
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-})
+package({ "glepnir/dashboard-nvim", config = conf.dashboard })
 
 package({
-  'lukas-reineke/indent-blankline.nvim',
-  --  event = 'BufRead',
-  config = conf.indent_blankline,
+	"glepnir/galaxyline.nvim",
+	branch = "main",
+	config = conf.galaxyline,
+	requires = { "kyazdani42/nvim-web-devicons", opt = true },
 })
 
 package({
-  'akinsho/bufferline.nvim',
-  config = conf.nvim_bufferline,
-  requires = 'kyazdani42/nvim-web-devicons',
+	"lukas-reineke/indent-blankline.nvim",
+	--  event = 'BufRead',
+	config = conf.indent_blankline,
 })
 
 package({
-  'kyazdani42/nvim-tree.lua',
-  cmd = 'NvimTreeToggle',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config = conf.nvim_tree,
+	"akinsho/bufferline.nvim",
+	config = conf.nvim_bufferline,
+	requires = "kyazdani42/nvim-web-devicons",
 })
 
 package({
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup()
-  end,
+	"kyazdani42/nvim-tree.lua",
+	cmd = "NvimTreeToggle",
+	requires = "kyazdani42/nvim-web-devicons",
+	config = conf.nvim_tree,
 })
 
-package({ 'mechatroner/rainbow_csv', ft = 'csv' })
+package({
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+	end,
+})
+
+package({ "mechatroner/rainbow_csv", ft = "csv" })
 -- package({ 'jupyter-vim/jupyter-vim' })
-package({ 'nvim-telescope/telescope-ui-select.nvim' })
+package({ "nvim-telescope/telescope-ui-select.nvim" })
