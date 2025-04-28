7dfac8f300538864cde38556507875a9dcd6c2f1
RakerZh <gitrustz10@gmail.com>
2025-04-28 20:07:01 +1000

    Add Neotree

--- lua/modules/tools/package.lua
+++ lua/modules/tools/package.lua
@@ -154,16 +154,16 @@ packadd({ 'jghauser/follow-md-links.nvim' })
 
 -- package({ 'RakerZh/min-preview.nvim' })
 
--- packadd({
---   'nvim-neo-tree/neo-tree.nvim',
---   branch = 'v3.x',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   config = conf.neotree,
--- })
+packadd({
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = conf.neotree,
+})
 
 packadd({
   'stevearc/oil.nvim',
