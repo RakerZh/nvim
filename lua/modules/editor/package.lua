070436ebbe61c63f4969821f4643336c6e61cb22
RakerZh <gitrustz10@gmail.com>
2025-09-01 23:38:28 +1000

    update

--- lua/modules/editor/package.lua
+++ lua/modules/editor/package.lua
@@ -30,6 +30,16 @@ packadd({
   },
 })
 
+-- jupyternotebook
+-- packadd({
+--   'benlubas/molten-nvim',
+--   version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
+--   build = ':UpdateRemotePlugins',
+--   init = function()
+--     vim.g.molten_output_win_max_height = 12
+--   end,
+-- })
+
 packadd({
   'LeonHeidelbach/trailblazer.nvim',
   config = function()
